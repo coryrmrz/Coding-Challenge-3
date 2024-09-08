79b6898fd2e4b75afe9196e5eb1f6d47e9f1352f
coryrmrz <coryramirez2018@gmail.com>
2024-09-08 02:25:42 -0400

    Implement generatePerformanceReport function

--- cc_3.js
+++ cc_3.js
@@ -23,17 +23,40 @@ function determinePerformanceRating (averageSales) {
 //Task 3: Create a function to identify top and bottom performers
 
 function findTopAndBottomPerformers (salesperson) {
-    if (salesperson.length === 0) return {topPerformer: null, bottomPerformer: null};
-    const totalSales = salesperson.map(person => person.totalSales);
+    if (salesperson.length === 0) return {topPerformer: null, bottomPerformer: null}; // salesperson name property
+    const totalSales = salesperson.map(person => person.totalSales); // total sales property
 
-    const maxSales = Math.max(...totalSales);
-    const minSales = Math.min(...totalSales);
+    const maxSales = Math.max(...totalSales); // Determines highest sales
+    const minSales = Math.min(...totalSales); // Determines lowest sales
 
-    const topPerformer = salesperson.find(person => person.totalSales === maxSales);
-    const bottomPerformer = salesperson.find(person => person.totalSales === minSales);
+    const topPerformer = salesperson.find(person => person.totalSales === maxSales); // Determines salesperosn with highest sales
+    const bottomPerformer = salesperson.find(person => person.totalSales === minSales); // Determines salesperson with lowest sales
     
     return {
         topPerformer,
-        bottomPerformer
+        bottomPerformer // returns top and bottom performer properties
+    };
+}
+
+//Task 4: Combine functions to generate a performance report
+
+function generatePerformanceReport (salesData) {
+    const salesperson = salesInfo.map(person => {
+        const averageSales = calculateAverageSales(person.sales); // compute average sales for each salesperson
+        const rating = determinePerformanceRating (averageSales); // assigns performance rating to each salesperson based on average sales
+        return {
+            name;person.name,
+            averageSales
+        };
+    });
+    const{topPerformer, bottomPerformer} = findTopAndBottomPerformer(salespeople); // identify top and bottom performer
+    return {
+        salesperson: salesperson.map(person => ({
+            name:person.name
+            averageSales: person.averageSales
+            performanceRating: person.rating // Summary of salesperson's average sales, performance rating, top and bottom performer
+        })),
+        topPerformer: topPerformer ? topPerformer.name : "None",
+        bottomPerformer: bottomPerformer ? bottomPerformer.name : "None"
     };
 }
