86bcfb3db2440d4648e8b915b3c4a7b5706d0170
coryrmrz <coryramirez2018@gmail.com>
2024-09-08 01:35:07 -0400

    Create determinePerformanceRating function

--- cc_3.js
+++ cc_3.js
@@ -5,3 +5,17 @@ function calculateAverageSales(sales) {
     const totalSales = sales.reduce((sum, sale) => sum + sale, 0);
     return totalSales / salesLength;
 }
+
+//Task 2: Create a function to determine performance rating
+
+function determinePerformanceRating (averageSales) {
+    if (averageSales > 10000) {
+        return "Excellent";
+    } else if (averageSales >= 7000) {
+        return "Good";
+    } else if (averageSales >= 4000) {
+        return "Satisfactory";
+    } else {
+        return "Needs Improvement";
+    }
+}
